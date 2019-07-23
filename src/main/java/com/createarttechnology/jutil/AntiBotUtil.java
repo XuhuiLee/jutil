cc38ed67962b3800d39f967a15cc2756ef7fdfc0
XuhuiLee <lxh199108@gmail.com>
2019-07-23 19:14:04 +0800

    增加几个简单的方法

--- src/main/java/com/createarttechnology/jutil/AntiBotUtil.java
+++ src/main/java/com/createarttechnology/jutil/AntiBotUtil.java
@@ -1,27 +1,40 @@
 package com.createarttechnology.jutil;
 
+
+import com.createarttechnology.constant.StringConstant;
+
 import javax.servlet.http.HttpServletRequest;
 
 /**
+ * 检测爬虫和脚本
  * Created by lixuhui on 2018/5/9.
  */
-public class AntiBotUtil {
+public final class AntiBotUtil {
+
+    private AntiBotUtil() {}
 
     /**
      * 根据request信息判断是否来自爬虫或脚本
      */
     public static boolean isBot(HttpServletRequest request) {
+        Boolean isBot = (Boolean) request.getAttribute(StringConstant.REQUEST_ATTRIBUTE_IS_BOT);
+        if (isBot != null) {
+            return isBot;
+        }
+
         String userAgent = request.getHeader("User-Agent");
         String uri = request.getRequestURI();
 
         // 空ua
         boolean isAjaxRequest = uri.startsWith("/ajax/");
         if (StringUtil.isEmpty(userAgent) && !isAjaxRequest) {
+            request.setAttribute(StringConstant.REQUEST_ATTRIBUTE_IS_BOT, true);
             return true;
         }
 
         // 包含特殊标识
         if (userAgent.length() < 8 || userAgent.contains("bot")) {
+            request.setAttribute(StringConstant.REQUEST_ATTRIBUTE_IS_BOT, true);
             return true;
         }
 
--- src/main/java/com/createarttechnology/jutil/CollectionUtil.java
+++ src/main/java/com/createarttechnology/jutil/CollectionUtil.java
@@ -2,13 +2,31 @@ package com.createarttechnology.jutil;
 
 import com.google.common.collect.Lists;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 /**
  * Created by lixuhui on 2018/9/18.
  */
 public class CollectionUtil {
+
+    public static boolean isEmpty(Collection collection) {
+        return collection == null || collection.isEmpty();
+    }
+
+    public static boolean isNotEmpty(Collection collection) {
+        return !isEmpty(collection);
+    }
+
+    public static boolean isEmpty(Map map) {
+        return map == null || map.isEmpty();
+    }
+
+    public static boolean isNotEmpty(Map map) {
+        return !isEmpty(map);
+    }
 
     public static <I, O> List<O> transformList(List<I> inputList, Function<I, O> function) {
         if (inputList == null)
